0028d8494a0eba2e15d20d2df6173d4d707d822f
Sebastian Bergmann <sb@sebastian-bergmann.de>
2017-12-26 14:57:41 +0100

    Initial work on Test, TestCollection, and TestCollectionIterator

---
+++ src/TestCollection.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of PHPUnit.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPUnit\Runner;
+
+final class TestCollection implements \IteratorAggregate
+{
+    /**
+     * @var Test[]
+     */
+    private $tests = [];
+
+    public function add(Test $test): void
+    {
+        $this->tests[] = $test;
+    }
+
+    /**
+     * @return Test[]
+     */
+    public function getItems(): array
+    {
+        return $this->tests;
+    }
+
+    public function getIterator(): TestCollectionIterator
+    {
+        return new TestCollectionIterator($this);
+    }
+}
